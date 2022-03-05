869c7f8a18fefae9daedb1f5025ba0ee66804314
anderson-cst <andersonleandrocosta@hotmail.com>
2022-03-05 13:25:21 -0300

    first commit

---
+++ src/main/java/com/itexperts/projeto/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.itexperts.projeto.controller;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itexperts.projeto.model.User;
+import com.itexperts.projeto.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user){
+		User u = new User();
+		u = userService.create(user);
+		
+		return ResponseEntity.ok().body(u);		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody User user, @PathVariable Long id){
+		userService.update(user, id);
+		
+		return ResponseEntity.noContent().build();		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(@PathVariable Long id){
+		User u = new User();
+		u = userService.getByid(id);
+		return ResponseEntity.ok().body(u);
+	}
+	
+	@GetMapping("/{list}")
+	public ResponseEntity<List<User>> getAllList(){
+		List<User> users = userService.getAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping("/{page}")
+	public ResponseEntity<Page<User>> getAllPage(Pageable pageable){
+		Page<User> users = userService.getAll(pageable);
+		return ResponseEntity.ok().body(users);
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/itexperts/projeto/service/UserService.java
@@ -0,0 +1,68 @@
+package com.itexperts.projeto.service;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itexperts.projeto.model.User;
+import com.itexperts.projeto.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public User create(User user) {
+		User u = new User();
+		u = userRepository.save(user);
+		
+		return u;
+	}
+	
+	@Transactional
+	public void update(User user, Long id) {
+		Optional<User> userReturned = userRepository.findById(id);
+		userReturned.orElseThrow(()-> new RuntimeException("User not found"));
+		
+		userReturned.get().setName(user.getName());
+		userReturned.get().setLastName(user.getLastName());
+		
+		userRepository.save(userReturned.get());
+	}
+	
+	@Transactional(readOnly = true)
+	public User getByid(Long id) {
+		Optional<User> userReturned = userRepository.findById(id);
+		userReturned.orElseThrow(()-> new RuntimeException("User not found"));
+		
+		return userReturned.get();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<User> getAll(){
+		List<User> users = userRepository.findAll();
+		
+		return users;		
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<User> getAll(Pageable pageable){
+		Page<User> users = userRepository.findAll(pageable);
+		return users;	
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
